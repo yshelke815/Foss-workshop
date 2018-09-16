7cb6adb80a8bff4a97a943d0f5e4c9639b1350b1
yshelke815 <yshelke815@gmail.com>
2018-09-16 10:59:12 +0530

    added text.sh file

---
+++ text.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Making foss folder"
+mkdir foss
+
+echo "going to foss folder"
+cd foss
+
+echo "Creating test_file.txt"
+touch test_file.txt
+
+echo ""
+echo"Showing output of ls:"
+ls
+
+echo ""
+echo"updateing linux repos"
